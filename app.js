ba0cd52ffb82b3512e8dc0928556b2e1f1cabe12
Mathieu Duchesneau <mathieu@doubledash.io>
2024-02-07 10:00:08 +0000

    process & parse data readouts in app.js + add dependencies in package.json, yarn.lock

--- app.js
+++ app.js
@@ -2,19 +2,63 @@
 import { SerialPort } from 'serialport';
 import { ReadlineParser } from '@serialport/parser-readline';
 
-const port = new SerialPort({
-  // windows: e.g. 'COM3';
-  path: '/dev/ttyACM0',
-  baudRate: 9600 ,
-});
+// windows: e.g. path = 'COM3';
+const [path, interval, baudRate ] = ['/dev/ttyACM0', 1000, 9600];
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+// open port
+const port = new SerialPort({
+  path,
+  baudRate,
+});
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+// write instructions
+port.write(Buffer.from('INFO\r\n'), err => { if (err) console.log('ERROR!', err) });
+port.drain(err => { delay(100) });
+port.write(Buffer.from(`POLL 1000\r\n`), err => { if (err) console.log('ERROR!', err) });
+port.drain(err => { delay(100) });
+port.write(Buffer.from('FRAC 2\r\n'), err => { if (err) console.log('ERROR!', err) });
+port.drain(err => { delay(100) });
 
 // read event
-parser.on('data', function(data) {
-  // handle any of the ports being unable to report data
-  data = data.replace('ERROR', '-1');
+let info_line, padlen;
+parser.on('data', async (data) => {
+  const split = data.replace(', ', ',').split(',');
+
+  // example info line:
+  // I,Product ID,Serial Number,Message,MS5611 Pressure,Pa,SHT31 Temperature,C,SHT31 Relative Humidity,%,*bbdd
+
+  // extract info line
+  if (split[0] === 'I') {
+    // parse field titles
+    if (split[1] == "Product ID") {
+      info_line = split
+      padlen = Math.max(...(split.slice(4).map(s => s.length)));
+      console.info(info_line.join(','))
+
+    // echo any other info lines
+    } else {
+      console.info(split[3]);
+    }
+    return;
+  }
+  if (!info_line) return console.info('Awaiting info line...');
+
+  // example readout line:
+  // D,VCP-PTH450-CAL,E24638,,102466,Pa,24.87,C,66.81,%,*d16d
+
+  // extract readout values
+  const device = `${split[1]} ${split[2]}`
+  const sensors = info_line.slice(4).filter((v, i) => i % 2 < 1);
+  const values = split.slice(4).filter((v, i) => i % 2 < 1).map(parseFloat);
+  const units = split.slice(4).filter((v, i) => i % 2 > 0);
 
   // print result
-  console.log(data);
+  console.info(`${new Date().toLocaleString('en-CA')} ${device}`)
+  for (const i in units) {  // `units` will have the shorter range (no *abcd value)
+    console.info(`${sensors[i].padEnd(padlen + 2)} ${values[i]} ${units[i]}`)
+  }
+  console.info('\n');
+
 });
